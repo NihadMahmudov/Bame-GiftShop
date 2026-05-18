334247e50dd5beec6a553aced0c22232dc0f0458
Nihad <mahmudovnihad537@gmail.com>
2026-05-18 23:27:26 +0400

    feat: add trust features bar and clean up stories logic

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Hero from '../../components/home/Hero';
+import FeatureBar from '../../components/home/FeatureBar';
 import FlashSale from '../../components/home/FlashSale';
 import RecentlyViewed from '../../components/home/RecentlyViewed';
 import GiftFinder from '../../components/home/GiftFinder';
-import StoryBar from '../../components/social/StoryBar/StoryBar';
 
 const Home = () => {
   return (
     <div className="home-page">
-      <StoryBar />
       <Hero />
+      <FeatureBar />
       <FlashSale />
       <RecentlyViewed />
       <GiftFinder />
